c656dae6b995cea247af525d81e8db1cf4616847
Chamu97 <majchemachandra@gmail.com>
2019-05-14 19:53:53 +0530

    added TypeDAO.java

---
+++ inventoryservice/src/main/java/com/sellnbye/inventoryservice/dao/TypeDAO.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package com.sellnbye.inventoryservice.dao;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.sellnbye.inventoryservice.model.Type;
+import com.sellnbye.inventoryservice.repository.TypeRepository;
+
+/**
+ * @author Janandhi Chamudika
+ * 
+ * DAO is the place where we insert, update, delete, get the data from the database
+ * using the typerepostitory and the model class
+ *
+ */
+public class TypeDAO {
+	
+	//create a object typerepostitory
+	@Autowired
+	TypeRepository typeRepository;
+
+	/*to save a type*/
+	
+	public Type save(Type typ) {
+		return typeRepository.save(typ);
+	}
+	
+	
+	/* search all types*/
+	
+	public List<Type> findAll(){
+		return typeRepository.findAll();
+	}
+	
+	
+	/*get a type by id*/
+	public Type findOne(Long typid) {
+		return typeRepository.findOne(typid);
+	}
+	
+	
+	/*delete a type*/
+	
+	public void delete(Type typ) {
+		typeRepository.delete(typ);
+	}
+	
+}
