592b8eefbed70def41d37209fc7d8f8ad390596f
Chamu97 <majchemachandra@gmail.com>
2019-05-18 15:15:58 +0530

    implemented viewtypes method

--- inventoryservice.zip_expanded/inventoryservice/src/main/java/com/sellnbye/inventoryservice/controller/AppController.java
+++ inventoryservice.zip_expanded/inventoryservice/src/main/java/com/sellnbye/inventoryservice/controller/AppController.java
@@ -1,8 +1,13 @@
 package com.sellnbye.inventoryservice.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.sellnbye.inventoryservice.model.Type;
 import com.sellnbye.inventoryservice.service.TypeService;
 
 @Controller
@@ -11,7 +16,13 @@ public class AppController {
 	@Autowired
 	TypeService typeService;
 	
-	public String viewTypes() {
+	@RequestMapping("/types")
+	/* Model use from spring mvc */
+	public String viewTypes(Model model) { 
+		
+		List<Type> listTypes = typeService.listAll();
+		model.addAttribute("listTypes", listTypes);
+		return "types";
 		
 	}
 	
